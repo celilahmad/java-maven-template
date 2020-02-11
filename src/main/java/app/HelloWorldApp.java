3e059932a9dc43b4e6edfdc897524ee12fdec49b
Celilkhan <celil.ahmad@gmail.com>
2020-02-11 13:38:32 +0400

    Addition

--- src/main/java/app/HelloWorldApp.java
+++ src/main/java/app/HelloWorldApp.java
@@ -1,9 +1,11 @@
 package app;
 
+
 public class HelloWorldApp {
 
   public static void main(String[] args) {
     System.out.println("Hello World");
+
   }
 
 }
---
+++ src/main/java/app/addition.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args) {
+        System.out.println("Enter your first number");
+        InputStream is = System.in;
+        Scanner sc = new Scanner(is);
+        int first = sc.nextInt();
+        System.out.println("Enter second number");
+        int second = sc.nextInt();
+        System.out.println(first+second);
+
+    }
+}
