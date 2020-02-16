d805cfd99c8a98e1096b82fede17153ccd5925da
Celilkhan <celil.ahmad@gmail.com>
2020-02-16 22:02:12 +0400

    Battleship

--- src/main/java/app/HelloWorldApp.java
+++ src/main/java/app/HelloWorldApp.java
@@ -9,7 +9,7 @@ public class HelloWorldApp {
 
 
         int height = 6;
-        int width = 5;
+        int width = 6;
         int[][] square = new int[height][width];
 
         int guess1 = 0;
@@ -21,38 +21,45 @@ public class HelloWorldApp {
         int[][] rand = new int[randomLine][randomColumn];
 
 
+
+
         //if (guess1=rand[][])
 
 
-        for (int i = 0; i <= width; i++) {
-            if (i == 0) {
-                System.out.print(i + "|");
-            } else {
-                System.out.print(" " + i + " |");
-            }
-        }
-        System.out.println();
-        for (int x = 1; x < square.length; x++) {
-            System.out.print(x);
-            for (int y = 0; y < square.length - 1; y++) {
 
-                System.out.print("| - ");
-            }
-
-            System.out.println("|");
-        }
 
         boolean con = true;
         Scanner sc = new Scanner(System.in);
         while (con){
-            System.out.println("Enter row");
+            for (int i = 0; i < width; i++) {
+                if (i == 0) {
+                    System.out.print(i + "|");
+                } else {
+                    System.out.print(" " + i + " |");
+                }
+            }
+            System.out.println();
+            for (int x = 1; x < square.length; x++) {
+                System.out.print(x);
+                for (int y = 0; y < square.length - 1; y++) {
+
+                    System.out.print("| - ");
+                }
+
+                System.out.println("|");
+            }
+            System.out.println("Enter row and column");
             guess1 = sc.nextInt();
-            System.out.println("Enter column");
+            //System.out.println("Enter column");
             guess2 = sc.nextInt();
+            int [][] array = new int[guess1][guess2];
+            if (array==rand){
+                System.out.println("you won");
+            }
 
             //int [][] array = new int[guess1][guess2];
 
-            if (guess1>0 && guess1 <6 && guess2>0 && guess2<6){
+         /*   if (guess1>0 && guess1 <6 && guess2>0 && guess2<6){
 
                 if (guess1==randomLine && guess2==randomColumn){
                     System.out.println("Congrats you won");
@@ -61,7 +68,7 @@ public class HelloWorldApp {
                     System.out.println("Try again");
 
                 }
-            }
+            }*/
 
 
         }
