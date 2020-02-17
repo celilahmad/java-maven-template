189223bd5b1d42e53265f221661f5f85478c1a0a
jalil <celil.ahmad@gmail.com>
2020-02-17 21:40:28 +0400

    Alpabet

---
+++ src/main/java/app/Alphabet.java
@@ -0,0 +1,23 @@
+package app;
+
+public class Alphabet {
+
+    public static void main(String[] args) {
+
+
+        //int [] range =new int [('Z' - 'A' +1)];
+        //int range1 = ('z' - 'a');
+
+
+
+
+        for(char i = 'a'; i <='z'; i++){
+            System.out.print(i);
+        }
+        System.out.println();
+
+        for(char i = 'A'; i <='Z'; i++){
+            System.out.print(i);
+        }
+    }
+}
