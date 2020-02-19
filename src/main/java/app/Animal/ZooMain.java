d2d8d826feec8378eb4273a246aa988656bd1b49
jalil <celil.ahmad@gmail.com>
2020-02-19 18:04:41 +0400

    Animal

--- src/main/java/app/Animal/ZooMain.java
+++ src/main/java/app/Animal/ZooMain.java
@@ -4,13 +4,15 @@ public class ZooMain {
 
     public static void main(String[] args) {
 
-        //Animal dog = new Animal();
+        Dog dog = new Dog();
+        dog.name= "Mick";
         Dog dog1 = new Dog();
         dog1.name="Toplan";
 
         Cat cat = new Cat();
         cat.name="Johny";
 
+        System.out.println(dog);
         System.out.println(dog1);
         System.out.println(cat);
 
